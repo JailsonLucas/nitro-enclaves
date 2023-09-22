3c1a40fd10931c1008f3ad6297e7ef00647c1ffc
Jailson Lucas <jailson@g4tech.com.br>
2023-09-22 11:03:36 -0300

    feat: add client service

---
+++ client/blockchain_client_service.py
@@ -0,0 +1,40 @@
+from web3 import Web3
+from contract_abi import ABI
+import json
+INFURA_URL_KEY = "https://sepolia.infura.io/v3/YOUR_KEY"
+SIGNER_ADDRESS = ""
+CLIENT_ADDRESS = ""
+ERC20_CONTRACT_ADDRESS = ""
+
+class BlockchainClientService:
+    def __init__(self):
+        self.web3 = Web3(Web3.HTTPProvider(INFURA_URL_KEY))
+
+
+    def get_ERC20_Contract(self):
+        web3 = self.web3
+        brl_contract = web3.eth.contract(address= ERC20_CONTRACT_ADDRESS, abi=ABI)
+        return brl_contract
+    
+
+    def deposit_transaction(self):
+        web3 = self.web3
+        brl_contract = self.get_ERC20_Contract()
+
+        deposit = brl_contract.functions.deposit(CLIENT_ADDRESS, 1000000000000000000)
+        deposit_tx= deposit.build_transaction({ 'from': SIGNER_ADDRESS, 'nonce': web3.eth.get_transaction_count(SIGNER_ADDRESS), 'gas': 200000, 'gasPrice': web3.to_wei('400', 'gwei') })
+        return deposit_tx
+    
+
+    def deposit(self):
+        data = self.deposit_transaction()
+        json_data = json.dumps(data)
+        return json_data
+    
+
+    def send_signed_transaction(self,signed_transaction):
+        transaction_hash=self.web3.eth.send_raw_transaction(signed_transaction)
+        return transaction_hash
+
+
+
---
+++ client/enclave_client.py
@@ -0,0 +1,85 @@
+import json
+import socket
+
+import requests
+from hexbytes import HexBytes
+import time
+
+from client.blockchain_client_service import BlockchainClientService
+
+ENCLAVE_PORT = 5000
+ENCLAVE_CID = 16
+
+class EnclaveClient:
+
+    def connect(self, endpoint):
+        self.sock = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
+        self.sock.connect(endpoint)
+
+
+    def send_data(self, data):
+        # Send data to a remote endpoint
+        data = data.encode('utf-8')
+        self.sock.sendall(data)
+
+
+    def recv_data(self):
+        # Receive data from a remote endpoint
+        data = ''
+        while len(data)<=0:
+            data = self.sock.recv(2048)
+            data = HexBytes(data)
+            if not data:
+                break
+            return data
+        
+
+    def disconnect(self):
+        # Close the client socket
+        self.sock.close()  
+        
+
+
+def get_aws_session_token():
+    # Get the AWS credential from EC2 instance metadata
+
+    r = requests.get("http://169.254.169.254/latest/meta-data/iam/security-credentials/")
+    instance_profile_name = r.text
+
+    r = requests.get("http://169.254.169.254/latest/meta-data/iam/security-credentials/%s" % instance_profile_name)
+    response = r.json()
+
+    return response
+   
+
+def client_handler():
+    client = EnclaveClient()
+    criptech_blockchain = BlockchainClientService()
+    endpoint = (ENCLAVE_CID, int(ENCLAVE_PORT))
+    time.sleep(3)
+    tx = criptech_blockchain.deposit_transaction()
+    response = get_aws_session_token()
+    data =  json.dumps({"credentials": {
+        'access_key_id' : response['AccessKeyId'],
+        'secret_access_key' : response['SecretAccessKey'],
+        'token' :response['Token'] },
+        "transaction": tx
+        })
+
+    client.connect(endpoint)
+    client.send_data(data)
+    signed_transaction_bytes = client.recv_data()
+
+    signed_transaction = HexBytes(signed_transaction_bytes)
+    transaction_hash_send = criptech_blockchain.send_signed_transaction(signed_transaction)
+    print(f"Transaction sent: {transaction_hash_send.hex()}")
+
+    client.disconnect()
+
+
+def main():
+    client_handler()
+
+
+if __name__ == "__main__":
+    main()
