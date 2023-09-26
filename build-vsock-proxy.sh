d8856e13f25d4781e3e23c8f0cca78dff98cfcc5
Jailson Lucas <jailson@g4tech.com.br>
2023-09-26 12:02:26 -0300

    fix: adjust build sockk

--- build-vsock-proxy.sh
+++ build-vsock-proxy.sh
@@ -1,4 +1,6 @@
 git clone https://github.com/aws/aws-nitro-enclaves-cli.git
 cd aws-nitro-enclaves-cli
 make vsock-proxy
-systemctl enable nitro-enclaves-vsock-proxy.service
+
+cd vsock_proxy/service
+sudo systemctl enable nitro-enclaves-vsock-proxy.service
